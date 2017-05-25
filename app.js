409fc01e0502e91eb1ddb917ceebb174a1de8812
cailu <cailu@cailudeMac-mini.lan>
2017-05-25 11:55:17 +0800

    add app

---
+++ app.js
@@ -0,0 +1,34 @@
+//app.js
+App({
+  onLaunch: function () {
+    //调用API从本地缓存中获取数据
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+  },
+  getUserInfo: function (cb) {
+    var that = this
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    } else {
+      //调用登录接口
+      wx.login({
+        success: function (res) {
+          // {
+          //   code: "021J0cTH1EPTT40Rz8TH1SomTH1J0cTs"
+          //   errMsg: "login:ok"
+          // }
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
+  },
+  globalData: {
+    userInfo: null
+  }
+})
